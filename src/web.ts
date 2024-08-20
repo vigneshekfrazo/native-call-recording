352425b3807b6d80e15c1cb617592575c9cd38d6
Vigneshwaran <vignesh@ekfrazo.in>
2024-08-20 22:43:49 +0530

    changes updated in web.ts file

--- src/web.ts
+++ src/web.ts
@@ -3,7 +3,7 @@ import { WebPlugin } from '@capacitor/core';
 import type { ExamplePlugin } from './definitions';
 
 export class ExampleWeb extends WebPlugin implements ExamplePlugin {
-  async initiateCall(options: { phoneNumber: string }): Promise<void> {
+  async initiateCall(_options: { phoneNumber: string }): Promise<void> {
     console.warn('This feature is not available on the web.');
   }
 }
